836cee1cf7738ce81b216438e5abe803fe948557
9r3i <luthfie@y7mail.com>
2023-10-30 04:53:06 +0700

    add bulk items

--- item.php
+++ item.php
@@ -1,22 +1,51 @@
 <?php
 $ini="
-1=2
-2=2
-3=2
-4=2
-5=2
-6=2
-
-545=2
-325=1
-
+; 42x Kelbi Horn
+327=42
+; 64x Genprey Fang
+357=64
 ";
 
 $req=parse_ini_string($ini);
-echo item($req,801);
-echo noitem(801,808);
+$res=item($req,701);
+$res.=noitem(701,702);
+
+$out=file_put_contents('ULUS10391.item.ini',$res);
+echo $out."\n";
 
 
+/* bulk items */
+function items(
+  int $start,   // start of block
+  int $len=1,   // length of block
+  int $init=1,  // initial of item
+  int $qt=1     // quantity of each item
+  ){
+  $s=0xd1c8;
+  $g=4;
+  $c=$start-1;
+  $t=$len+$c;
+  $a=dx($s+($g*$c));
+  $b=dx(($s+2)+($g*$c));
+  $l=dx($len);
+  $v=dx($init);
+  $m=dx($qt);
+  /* activation code */
+  $res="_C0 {$qt}x{$len}={$init} at {$start}-{$t} in Box\n";
+  $res.="_L 0x8119{$a} 0x{$l}0002\n";
+  $res.="_L 0x1000{$v} 0x00000001\n";
+  $res.="_L 0x8119{$b} 0x{$l}0004\n";
+  $res.="_L 0x0000{$m} 0x00000000\n";
+  /* reverse codes */
+  $res.="_C0 No Items at {$start}-{$t} in Box\n";
+  $res.="_L 0x8119{$a} 0x{$l}0002\n";
+  $res.="_L 0x10000000 0x00000001\n";
+  $res.="_L 0x8119{$b} 0x{$l}0004\n";
+  $res.="_L 0x00000000 0x00000000\n";
+  return $res;
+}
+
+/* remove items */
 function noitem(int $from=1,int $to=1){
   $to=$to>$from?$to:$from;
   $g=4;
@@ -39,6 +68,7 @@ function noitem(int $from=1,int $to=1){
   return $res;
 }
 
+/* specific items */
 function item(array $req,int $start=1){
   $g=4;
   $c=$start-1;
@@ -60,8 +90,7 @@ function item(array $req,int $start=1){
   return $res;
 }
 
+/* decimal to hexa */
 function dx(int $d,int $l=4){
   return strtoupper(sprintf('%0'.$l.'s',dechex($d)));
 }
-
-
